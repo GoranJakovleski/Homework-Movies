ea2c82aadf50d9ee1fccb62c13b3e7d8a95ee695
GoranJakovleski <zdravotoner@gmail.com>
2020-05-10 21:46:19 +0200

    Add project files.

---
+++ LINQApp/Helpers/MoviesHelper.cs
@@ -0,0 +1,39 @@
+﻿using LINQApp.Entities;
+using System.Collections.Generic;
+
+namespace LINQApp.Helpers
+{
+    public class MoviesHelper
+    {
+        public static List<Movie> MyMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie(){ Title = "The Mountain II", Year = 2016, Rating = 8.9f, Duration = 135 },
+                new Movie(){ Title = "Seven Samurai", Year = 1954, Rating = 8.6f, Duration = 207 },
+                new Movie(){ Title = "Big Hero 6", Year = 2014, Rating = 7.8f, Duration = 102 },
+                new Movie(){ Title = "The Man from Nowhere", Year = 2010, Rating = 7.8f, Duration = 119 },
+                new Movie(){ Title = "Gladiator", Year = 2000, Rating = 8.5f, Duration = 155 },
+                new Movie(){ Title = "Avengers: Endgame", Year = 2009, Rating = 8.4f, Duration = 181 },
+                new Movie(){ Title = "Kung Fu Hustle", Year = 2004, Rating = 7.7f, Duration = 99 },
+                new Movie(){ Title = "Iron Man", Year = 2008, Rating = 7.9f, Duration = 126 },
+                new Movie(){ Title = "Big Hero 6", Year = 2014, Rating = 7.8f, Duration = 102 },
+                new Movie(){ Title = "The Dark Knight", Year = 2008, Rating = 9.0f, Duration = 152 },
+                new Movie(){ Title = "13 Assassins", Year = 2010, Rating = 7.6f, Duration = 141 },
+                new Movie(){ Title = "Grindhouse", Year = 2007, Rating = 7.5f, Duration = 191 },
+
+                new Movie(){ Title = "Ben-Hur", Year = 1959, Rating = 8.1f, Duration = 212 },
+                new Movie(){ Title = "Ladyhawke", Year = 1985, Rating = 7.0f, Duration = 121 },
+                new Movie(){ Title = "Police Academy", Year = 1984, Rating = 6.7f, Duration = 96 },
+                new Movie(){ Title = "Avatar", Year = 2009, Rating = 7.8f, Duration = 162 },
+                new Movie(){ Title = "Rocky Balboa", Year = 2006, Rating = 6.7f, Duration = 102 },
+                new Movie(){ Title = "Scarface", Year = 2006, Rating = 8.3f, Duration = 98 },
+                new Movie(){ Title = "The Godfather", Year = 1976, Rating = 9.2f, Duration = 175 },
+                new Movie(){ Title = "Superman", Year = 1978, Rating = 7.3f, Duration = 143 },
+                new Movie(){ Title = "Need for Speed", Year = 2014, Rating = 6.5f, Duration = 132 },
+                new Movie(){ Title = "Madagascar", Year = 2005, Rating = 6.9f, Duration = 86 },
+                new Movie(){ Title = "Last Samurai", Year = 2003, Rating = 7.7f, Duration = 154 }
+            };
+        }
+    }
+}
---
+++ LINQApp/Program.cs
@@ -0,0 +1,116 @@
+﻿using LINQApp.Entities;
+using LINQApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Movie> movies = MoviesHelper.MyMovies();
+
+
+
+                // ****** Find all movies that their titles starts with "L"
+
+                var startingWith_T = movies
+                                    .Where(movie => movie.Title.StartsWith('L'))
+                                    .ToList();
+
+
+                //startingWith_T.ForEach(movie => Console.WriteLine(movie.Title));
+
+                // ***** Find the NUMBER of movies that have rating higher than 7.5
+
+                var moviesRatingHigher75 = movies
+                                          .Where(movie => movie.Rating > 7.5f)
+                                          .ToList();
+
+                //moviesRatingHigher75.ForEach(movie => Console.WriteLine(movie.Title));
+
+                // ****** Find all movies TITLES with year of production before 2005
+
+                var movieBefore2005 = movies
+                                     .Where(movie => movie.Year < 2005)
+                                     .ToList();
+
+                // movieBefore2005.ForEach(movie => Console.WriteLine(movie.Title));
+                
+
+                // ***** Find all movies TITLES and RATING that have rating higher then 8.0
+
+                var moviesRatingHigher80 = movies
+                                          .Where(movie => movie.Rating > 8.0f)
+                                          .ToList();
+
+                //moviesRatingHigher80.ForEach(movie => Console.WriteLine(movie.Title));
+            
+
+                // ***** Find first 5 movies that have duration time longer then 2 hours
+
+                var first5MoviesLongerthen120 = movies
+                                               .Where(movie => movie.Duration > 120)
+                                               .Take(5)
+                                               .ToList();
+                //first5MoviesLongerthen120.ForEach(movie => Console.WriteLine(movie.Title));
+            
+
+
+                // ****** Find last 3 movies TITLES and DURATION that have duration less then 2 hours
+
+                var last3Moviesless120 = movies
+                                        .Where(movie => movie.Duration < 120)
+                                        .TakeLast(3)
+                                        .ToList();
+                //last3Moviesless120.ForEach(movie => Console.WriteLine(movie));
+
+
+                // ***** Find all movies TITLES and RATING and order them by DURATION(DESC) -no condition needed
+
+                var allTitlesRatingDesc = movies
+                                         .OrderByDescending(movie => movie.Duration)
+                                         .Select(movie => new { movie.Title, movie.Rating })
+                                         .ToList();
+                //allTitlesRatingDesc.ForEach(movie => Console.WriteLine(movie));
+
+                // ****** Find all movies with TITLES that don't start with A and TITLES include more than 7 characters
+
+                var titlesDontStartAmore7 = movies
+                                           .Where(movie => !movie.Title.StartsWith('A'))
+                                           .Where(movie => movie.Title.Count() > 7)
+                                           .ToList();
+                //titlesDontStartAmore7.ForEach(movie => Console.WriteLine(movie.Title));
+               
+
+                // ****** Find all movies RATINGS that have RATINGS higher than 7.2, DURATIONS less then 1hour and 50min
+
+                var allRatings72Durationsless110 = movies
+                                                  .Where(movie => movie.Rating > 7.2f)
+                                                  .Where(movie => movie.Duration < 110)
+                                                  .ToList();
+                //allRatings72Durationsless110.ForEach(movie => Console.WriteLine(movie.Title));
+
+
+                // ****** Find all movies TITLES and RATINGS that have TITLES with less than 10 characters, DURATION
+                //        longer than 2 hours, RATING higher then 6.7 and order them by TITLE(ASC)
+           
+                var charactersDuranionRatingTitle = movies
+                                                   .Where(movie => movie.Title.Count() < 10)
+                                                   .Where(movie => movie.Duration > 120)
+                                                   .Where(movie => movie.Rating > 6.7f)
+                                                   .OrderBy(movie => movie.Title)
+                                                   .ToList();
+                //charactersDuranionRatingTitle.ForEach(movie => Console.WriteLine(movie.Title));
+
+
+
+
+                Console.ReadLine();
+            
+        }
+    }
+}
